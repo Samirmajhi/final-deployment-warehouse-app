9c77145671b5f35d1c5a9525c4862cc62e99f452
samir <2101020858@cgu-odisha.ac.in>
2024-07-02 16:35:06 +0545

    this should work let's see

--- app.py
+++ app.py
@@ -642,5 +642,5 @@ def handle_camera(selected_status, response):
             cap.release()
             cv2.destroyAllWindows()
 
-if __name__ =='__main__':
-   app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
