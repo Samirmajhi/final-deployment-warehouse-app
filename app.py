ba89d98c3a34289b9e0715dd18947c7a657e01c6
samir <2101020858@cgu-odisha.ac.in>
2024-07-03 20:48:17 +0545

    good finalsd

--- app.py
+++ app.py
@@ -16,7 +16,12 @@ from flask import Flask, request, jsonify, redirect,send_from_directory,session
 import mysql.connector
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+
+
 app = Flask(__name__,static_url_path='/static')
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 CORS(app)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
