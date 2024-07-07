ef5afd949bdf62371a2acba3abb8a1f6984c1ed8
samir <2101020858@cgu-odisha.ac.in>
2024-07-07 22:39:58 +0545

    what is this

--- app.py
+++ app.py
@@ -787,5 +787,5 @@ def process_barcode():
 #         if 'db_connection' in locals():
 #             db_connection.close()
 
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(debug=True,host='0.0.0.0')
